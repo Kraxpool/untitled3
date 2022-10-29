817711b7e5ffa758a2695d0b7d00e8975ad5f751
student <popihacvova@gmail.com>
2022-10-29 14:18:24 +0300

    work

---
+++ hm_1.py
@@ -0,0 +1,10 @@
+print(text)
+wordsText = input('words->')
+words = wordsText.split()
+print(words)
+for word in words:
+    if text.count(word)>0:
+        origen = word.upper()
+
+        text = text.replace(origen,teep)
+print(text)
